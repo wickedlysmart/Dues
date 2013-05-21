c67619b30a2c4d1801cd0949d30466f1b24bd9de
SungJae Lee <coffeetoast@LEEui-MacBook-Pro.local>
2013-05-21 12:35:51 +0900

    Initial Commit

---
+++ Dues/DuesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DuesAppDelegate.h
+//  Dues
+//
+//  Created by SungJae Lee on 13. 5. 21..
+//  Copyright (c) 2013년 SungJae Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DuesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dues/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dues
+//
+//  Created by SungJae Lee on 13. 5. 21..
+//  Copyright (c) 2013년 SungJae Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DuesAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DuesAppDelegate class]));
+    }
+}
